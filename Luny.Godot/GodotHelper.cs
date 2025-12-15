a4cc2ec16a5eb612200f376018028b09c972d9c0
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-12-15 21:15:16 +0100

    add godothelper, remove some logs

---
+++ Luny.Godot/GodotHelper.cs
@@ -0,0 +1,15 @@
+﻿using Godot;
+using System;
+
+namespace Luny.Godot
+{
+	public static class GodotHelper
+	{
+		public static String ToNotificationString(Int64 notificationId) => notificationId switch
+		{
+			Node.NotificationCrash => nameof(Node.NotificationCrash),
+			Node.NotificationWMCloseRequest => nameof(Node.NotificationWMCloseRequest),
+			var _ => notificationId.ToString(),
+		};
+	}
+}
--- Luny.Godot/GodotLifecycleAdapter.cs
+++ Luny.Godot/GodotLifecycleAdapter.cs
@@ -3,77 +3,66 @@ using System;
 
 namespace Luny.Godot
 {
-    /// <summary>
-    /// Ultra-thin Godot adapter: auto-initializes and forwards lifecycle to EngineLifecycleDispatcher.
-    /// </summary>
-    /// <remarks>
-    /// Gets auto-instantiated by LunyBootstrap.gd which itself gets auto-added to autoload via plugin.gd.
-    /// </remarks>
-    internal sealed partial class GodotLifecycleAdapter : Node
-    {
-        private static GodotLifecycleAdapter _instance;
+	/// <summary>
+	/// Ultra-thin Godot adapter: auto-initializes and forwards lifecycle to EngineLifecycleDispatcher.
+	/// </summary>
+	/// <remarks>
+	/// Gets auto-instantiated through autoload, added via plugin.gd.
+	/// </remarks>
+	internal sealed partial class GodotLifecycleAdapter : Node
+	{
+		private static GodotLifecycleAdapter _instance;
 
-        private IEngineLifecycleDispatcher _dispatcher;
+		private IEngineLifecycleDispatcher _dispatcher;
 
-        private GodotLifecycleAdapter()
-        {
-            if (_instance != null)
-            {
-                Throw.LifecycleAdapterSingletonDuplicationException(nameof(GodotLifecycleAdapter),
-                    _instance.Name, unchecked((Int64)_instance.GetInstanceId()), Name, unchecked((Int64)GetInstanceId()));
-            }
+		private GodotLifecycleAdapter()
+		{
+			if (_instance != null)
+			{
+				Throw.LifecycleAdapterSingletonDuplicationException(nameof(GodotLifecycleAdapter),
+					_instance.Name, unchecked((Int64)_instance.GetInstanceId()), Name, unchecked((Int64)GetInstanceId()));
+			}
 
-            _instance = this;
-            _dispatcher = EngineLifecycleDispatcher.Instance;
-        }
+			_instance = this;
+			_dispatcher = EngineLifecycleDispatcher.Instance;
+		}
 
-        public override void _Ready() => GD.Print($"GodotLifecycleAdapter ({GetInstanceId()}) _Ready");
+		public override void _Process(Double delta) => _dispatcher?.OnUpdate(delta);
 
-        public override void _Process(Double delta) => _dispatcher?.OnUpdate(delta);
+		public override void _PhysicsProcess(Double delta) => _dispatcher?.OnFixedStep(delta);
 
-        public override void _PhysicsProcess(Double delta) => _dispatcher?.OnFixedStep(delta);
+		public override void _ExitTree()
+		{
+			// we should not exit tree with an existing instance (indicates manual removal)
+			if (_instance != null)
+			{
+				Shutdown();
+				Throw.LifecycleAdapterPrematurelyRemovedException(nameof(GodotLifecycleAdapter));
+			}
+		}
 
-        public override void _ExitTree()
-        {
-            GD.Print($"GodotLifecycleAdapter ({GetInstanceId()}) _ExitTree");
-            if (_instance != null)
-            {
-                Shutdown();
-                Throw.LifecycleAdapterPrematurelyRemovedException(nameof(GodotLifecycleAdapter));
-            }
-        }
+		public override void _Notification(Int32 what)
+		{
+			if (what != NotificationProcess && what != NotificationPhysicsProcess)
+				GD.Print($"GodotLifecycleAdapter ({GetInstanceId()}) _Notification: {GodotHelper.ToNotificationString(what)}");
 
-        public override void _Notification(Int32 what)
-        {
-            switch ((Int64)what)
-            {
-                case NotificationCrash:
-                case NotificationWMCloseRequest:
-                    GD.Print($"GodotLifecycleAdapter ({GetInstanceId()}) _Notification: {GetNotificationString(what)}");
-                    Shutdown();
-                    break;
+			switch ((Int64)what)
+			{
+				case NotificationCrash:
+				case NotificationWMCloseRequest:
+					Shutdown();
+					break;
+			}
+		}
 
-                default:
-                    if (what != NotificationProcess && what != NotificationPhysicsProcess)
-                        GD.Print($"GodotLifecycleAdapter ({GetInstanceId()}) _Notification: {GetNotificationString(what)}");
-                    break;
-            }
-        }
-
-        private String GetNotificationString(Int32 what) => (Int64)what switch
-        {
-            NotificationWMCloseRequest => "NotificationWMCloseRequest",
-            var _ => what.ToString(),
-        };
-
-        private void Shutdown()
-        {
-            if (_instance != null)
-            {
-                _dispatcher?.OnShutdown();
-                _dispatcher = null;
-                _instance = null;
-            }
-        }
-    }
+		private void Shutdown()
+		{
+			if (_instance != null)
+			{
+				_dispatcher?.OnShutdown();
+				_dispatcher = null;
+				_instance = null;
+			}
+		}
+	}
 }
