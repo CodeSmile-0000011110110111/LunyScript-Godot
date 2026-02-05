79c64a0d404cf5dac006dbbf296c9626e265b790
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-02-05 13:45:46 +0100

    removed the event wrappers, that's overdoing it

--- plugin.gd
+++ plugin.gd
@@ -71,39 +71,22 @@ func GetCsprojPath() -> String:
     return ""
 
 
-## Wrapped Signals ...
-func OnEnablePlugin() -> void:
-    AddLunyEngineAutoload()
-    EnsureAnalyzerReferencedInCsproj()
-
-func OnDisablePlugin() -> void:
-    RemoveLunyEngineAutoload()
-
-func OnEnterTree() -> void:
-    AddLunyEngineAutoload()
-    EnsureAnalyzerReferencedInCsproj()
-
-func OnBuild() -> void:
-    EnsureAnalyzerReferencedInCsproj()
-
-func OnEditorReceiveFocus() -> void:
-    EnsureAnalyzerReferencedInCsproj()
-
-
 ## GDScript Signals ...
 func _enable_plugin() -> void:
-    OnEnablePlugin()
+    AddLunyEngineAutoload()
+    EnsureAnalyzerReferencedInCsproj()
 
 func _disable_plugin() -> void:
-    OnDisablePlugin()
+    RemoveLunyEngineAutoload()
 
 func _enter_tree() -> void:
-    OnEnterTree()
+    AddLunyEngineAutoload()
+    EnsureAnalyzerReferencedInCsproj()
+
+func _build() -> bool:
+    EnsureAnalyzerReferencedInCsproj()
+    return true
 
 func _notification(what: int) -> void:
     if what == NOTIFICATION_APPLICATION_FOCUS_IN:
-        OnEditorReceiveFocus()
-
-func _build() -> bool:
-    OnBuild()
-    return true
+        EnsureAnalyzerReferencedInCsproj()
